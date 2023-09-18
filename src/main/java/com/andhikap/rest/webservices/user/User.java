854263f4e408bf1797800fed0f2f0669fc6415b3
Andhikap <prameswaara@gmail.com>
2023-09-18 10:44:25 +0700

    connection to h2 database

--- src/main/java/com/andhikap/rest/webservices/user/User.java
+++ src/main/java/com/andhikap/rest/webservices/user/User.java
@@ -1,11 +1,17 @@
 package com.andhikap.rest.webservices.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 1, message = "Character of name should have at least 2 character.")
     private String name;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user_details (id, birth_date, name)
+VALUES (12, CURRENT_DATE(), 'prameswara');
